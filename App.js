6d204b1e111e20c8825d78bb4e9a9aa6b70f6938
emmyjaff <emmyjaff22@gmail.com>
2023-01-13 21:17:02 +0100

    modals

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
+        initialRouteName="Home"
         // screenOptions={{headerShown: false}}
         screenOptions={({route}) => ({
           // headerShown: false,
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,61 @@
-import * as React from 'react';
-import {Button, View, Text} from 'react-native';
+import React from 'react';
+import {Button, Modal, Center, NativeBaseProvider} from 'native-base';
+import {useState} from 'react';
 
-export default function HomeScreen({navigation}) {
+const Example = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
+    <Center>
+      <Button onPress={() => setShowModal(true)}>Button</Button>
+      <Modal
+        isOpen={showModal}
+        onClose={() => setShowModal(false)}
+        _backdrop={{
+          _dark: {
+            bg: 'coolGray.800',
+          },
+          bg: 'warmGray.50',
+        }}>
+        <Modal.Content maxWidth="350" maxH="412">
+          <Modal.CloseButton />
+          <Modal.Header>Return Policy</Modal.Header>
+          <Modal.Body>
+            Create a 'Return Request' under “My Orders” section of App/Website.
+            Follow the screens that come up after tapping on the 'Return’
+            button. Please make a note of the Return ID that we generate at the
+            end of the process. Keep the item ready for pick up or ship it to us
+            basis on the return mode.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="ghost"
+                colorScheme="blueGray"
+                onPress={() => {
+                  setShowModal(false);
+                }}>
+                Cancel
+              </Button>
+              <Button
+                onPress={() => {
+                  setShowModal(false);
+                }}>
+                Save
+              </Button>
+            </Button.Group>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
+    </Center>
   );
-}
+};
+
+export default () => {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+        <Example />
+      </Center>
+    </NativeBaseProvider>
+  );
+};
